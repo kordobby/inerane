1f91e1630cbc44bada0c1755842bc767e5a195db
kordobby <poordobby@naver.com>
2022-06-07 01:58:50 +0900

    Devtool 추가

--- src/redux/configStore.jsx
+++ src/redux/configStore.jsx
@@ -3,10 +3,10 @@
 import { createStore, applyMiddleware } from  "redux";
 import rootReducer from './modules';
 import thunk from 'redux-thunk';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middlewares = [thunk];
-const enhancer = applyMiddleware(...middlewares);
+const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
 const store = createStore(rootReducer, enhancer);
 
 export default store;
