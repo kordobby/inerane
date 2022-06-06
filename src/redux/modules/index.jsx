dd712ffc52f17a8ab43ed827fb76830908432520
kordobby <poordobby@naver.com>
2022-06-07 01:58:39 +0900

    좋아요 리듀서 추가

--- src/redux/modules/index.jsx
+++ src/redux/modules/index.jsx
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import boardReducer from './boardReducer';
 import userReducer from './userReducer';
+import likeReducer from './likeReducer';
 
-const rootReducer = combineReducers ({ boardReducer, userReducer });
+
+const rootReducer = combineReducers ({ boardReducer, userReducer, likeReducer });
 
 export default rootReducer;
